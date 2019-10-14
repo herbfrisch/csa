144c98016859ef6b704040e9f8f62260d44a570f
Jörg Bergmann <joergber@energing.de>
2019-10-14 18:01:23 +0200

    Add files via upload

---
+++ csa-uebung/07_TemplateEngines/app_templateengines01_templatestrings/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const movieRouter = require('./movie');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (request, response) => response.redirect('/movie'));
+
+app.use(morgan('common', { immediate: true }));
+
+app.use('/movie', movieRouter);
+
+app.listen(8080, () => {
+  console.log('Server is listening to http://localhost:8080');
+});
---
+++ csa-uebung/07_TemplateEngines/app_templateengines01_templatestrings/movie/controller.js
@@ -0,0 +1,43 @@
+const model = require('./model');
+const view = require('./view');
+const form = require('./form');
+
+function listAction(request, response) {
+  const movies = model.getAll();
+  const body = view(movies);
+  response.send(body);
+}
+
+function deleteAction(request, response) {
+  const id = parseInt(request.params.id, 10);
+  model.delete(id);
+  response.redirect(request.baseUrl);
+}
+
+function formAction(request, response) {
+  let movie = { id: '', title: '', year: '' };
+
+  if (request.params.id) {
+    movie = model.get(parseInt(request.params.id, 10));
+  }
+
+  const body = form(movie);
+  response.send(body);
+}
+
+function saveAction(request, response) {
+  const movie = {
+    id: request.body.id,
+    title: request.body.title,
+    year: request.body.year,
+  };
+  model.save(movie);
+  response.redirect(request.baseUrl);
+}
+
+module.exports = {
+  listAction,
+  deleteAction,
+  formAction,
+  saveAction,
+};
