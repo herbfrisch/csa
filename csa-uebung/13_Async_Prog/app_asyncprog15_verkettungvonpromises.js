a787467b5f00c7fd2cb3515efb53365f20352a3f
Jörg Bergmann <joergber@energing.de>
2019-11-29 08:28:01 +0100

    Add files via upload

---
+++ csa-uebung/13_Async_Prog/app_asyncprog15_verkettungvonpromises.js
@@ -0,0 +1,21 @@
+function asyncOperation(resolve, value, time) {
+  return new Promise((res, rej) => {
+    setTimeout(() => (resolve ? res(value) : rej(value)), time);
+  });
+}
+
+asyncOperation(true, 'Hello', 100)
+  .then(data => {
+    console.log(data + ' ');
+    return asyncOperation(true, 'World', 200);
+  })
+  .then(data => {
+    console.log(data);
+    return '!';
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => {
+    console.log('Error: ', err);
+  });
